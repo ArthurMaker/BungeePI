2e817253476b7714fb13fc68983ca68d6e62d66f
Goblom <goblom@me.com>
2013-11-26 18:49:43 -0800

    Finished CommandController, not tested yet, but should work in theory

--- BPI-Bukkit_Example/src/main/java/org/goblom/bpi/bukkit_example/utils/CommandController.java
+++ BPI-Bukkit_Example/src/main/java/org/goblom/bpi/bukkit_example/utils/CommandController.java
@@ -24,8 +24,14 @@
 
 package org.goblom.bpi.bukkit_example.utils;
 
+import java.io.IOException;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.goblom.bpi.bukkit.BungeePI;
+import org.goblom.bpi.bukkit.controller.BungeePlayer;
+import org.goblom.bpi.bukkit_example.BPIExample;
 
 /**
  *
@@ -33,35 +39,98 @@ import org.bukkit.command.CommandSender;
  */
 public class CommandController {
     
-    public static void addServer(CommandSender sender, Command command, String label, String[] args){
-        
+    public static void addServer(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 2) {
+            BungeePI.getPlugin().getController().addBungeeServer(args[1]);
+            sender.sendMessage(BPIExample.prefix + ChatColor.DARK_GREEN + "Successfully added server " + ChatColor.GOLD + args[1] + ChatColor.DARK_GREEN + ".");
+        } else sender.sendMessage(BPIExample.prefix + ChatColor.RED + "You are missing the server, or have too many arguments.");
     }
     
-    public static void getServer(CommandSender sender, Command command, String label, String[] args){
-        
+    public static void getServer(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            if (args.length == 1) {
+                try {
+                    String server = BungeePI.getPlugin().getController().getBungeePlayer(sender.getName()).getServer();
+                    sender.sendMessage(BPIExample.prefix + ChatColor.DARK_GREEN + "You are in server " + ChatColor.GOLD + server);
+                } catch (IOException e) { e.printStackTrace(); }
+            } else sender.sendMessage(BPIExample.prefix + ChatColor.RED + "Too many arguments.");
+        } else sender.sendMessage(BPIExample.prefix + "Only players can use this command.");
     }
     
-    public static void playerList(CommandSender sender, Command command, String label, String[] args){
-        
+    public static void playerList(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 2) {
+            try {
+                String playerList = BungeePI.getPlugin().getController().getBungeeServer(args[1]).getPlayerList();
+                sender.sendMessage(BPIExample.prefix + ChatColor.DARK_GREEN + "Players for server... " + ChatColor.GOLD + args[1]);
+                sender.sendMessage(BPIExample.prefix + ChatColor.LIGHT_PURPLE + playerList);
+            } catch (IOException e) { e.printStackTrace(); }
+        } else if (args.length == 1) {
+            try {
+                String server = BungeePI.getPlugin().getController().getBungeePlayer(sender.getName()).getServer();
+                String playerList = BungeePI.getPlugin().getController().getBungeeServer(server).getPlayerList();
+                
+                sender.sendMessage(BPIExample.prefix + ChatColor.DARK_GREEN + "Players for server... " + ChatColor.GOLD + server);
+                sender.sendMessage(BPIExample.prefix + ChatColor.LIGHT_PURPLE + playerList);
+            } catch (IOException e) { e.printStackTrace(); }
+        } else sender.sendMessage(BPIExample.prefix + ChatColor.RED + "You are missing the server, or have too many arguments.");
     }
     
-    public static void findPlayer(CommandSender sender, Command command, String label, String[] args){
-        
+    public static void findPlayer(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 2) {
+            try {
+                String server = BungeePI.getPlugin().getController().getBungeePlayer(args[1]).getServer();
+                sender.sendMessage(BPIExample.prefix + ChatColor.DARK_GREEN + args[1] + " is in " + server);
+            } catch (IOException e) { e.printStackTrace(); }
+        } else sender.sendMessage(BPIExample.prefix + ChatColor.RED + "You are missing the player, or have too many arguments.");
     }
     
-    public static void getIP(CommandSender sender, Command command, String label, String[] args){
-        
+    public static void getIP(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 2) {
+            try {
+                String ip = BungeePI.getPlugin().getController().getBungeePlayer(args[1]).getIP();
+                sender.sendMessage(BPIExample.prefix + ChatColor.DARK_GREEN + args[1] + "'s ip is " + ip);
+            } catch (Exception e) { e.printStackTrace(); }
+        } else sender.sendMessage(BPIExample.prefix + ChatColor.RED + "You are missing the player, or have too many arguments.");
     }
     
-    public static void playerCount(CommandSender sender, Command command, String label, String[] args){
-        
+    public static void playerCount(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 2) {
+            try {
+                int playerCount = BungeePI.getPlugin().getController().getBungeeServer(args[1]).getPlayerCount();
+                sender.sendMessage(BPIExample.prefix + ChatColor.DARK_GREEN + "Therea are " + playerCount + " in server " + args[1]);
+            } catch (IOException e) { e.printStackTrace(); }
+        } else if (args.length == 1) {
+            try {
+                String server = BungeePI.getPlugin().getController().getBungeePlayer(sender.getName()).getServer();
+                int playerCount = BungeePI.getPlugin().getController().getBungeeServer(server).getPlayerCount();
+                sender.sendMessage(BPIExample.prefix + ChatColor.DARK_GREEN + "Therea are " + playerCount + " in server " + server);
+            } catch (IOException e) { e.printStackTrace(); }
+        } else sender.sendMessage(BPIExample.prefix + ChatColor.RED + "You are missing the server, or have too many arguments.");
     }
     
-    public static void send(CommandSender sender, Command command, String label, String[] args){
-        
+    public static void send(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 3) {
+            try {
+                BungeePI.getPlugin().getController().getBungeePlayer(args[1]).moveToServer(args[2]);
+            } catch (IOException e) { e.printStackTrace(); }
+        } else sender.sendMessage(BPIExample.prefix + ChatColor.RED + "You are missing the server, the player, or have too many arguments.");
     }
     
-    public static void sendMessage(CommandSender sender, Command command, String label, String[] args){
-        
+    public static void sendMessage(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length >= 3) {
+            try {
+                StringBuilder message = new StringBuilder();
+                for (int i = 1; i < args.length; i++) {
+                    message.append(args[i]);
+                    message.append(" ");
+                }
+                
+                BungeePI.getPlugin().getController().getBungeePlayer(args[1]).sendMessage(
+                        message.toString()
+                                .replaceFirst(args[1], "")
+                                .replaceFirst(args[2], "")
+                );
+            } catch (IOException e) { e.printStackTrace(); }
+        } else sender.sendMessage(BPIExample.prefix + ChatColor.RED + "You are missing the player, or you don't enough arguments.");
     }
 }
